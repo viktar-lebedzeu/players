ce911a5eadd2cf6a019dab90afe13a151bfdd306
Viktar Lebedzeu <vlebedzeu@gmail.com>
2020-05-03 13:55:44 +0300

    Initial commit

---
+++ src/main/java/org/vlebedzeu/players/Application.java
@@ -0,0 +1,17 @@
+package org.vlebedzeu.players;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String... args) {
+        logger.info("Running Players application...");
+        logger.info("Done.");
+    }
+}
